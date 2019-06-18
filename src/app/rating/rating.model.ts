ab7304054e5e3428358bf30079836719250c9ff1
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-06-18 14:27:39 +0530

    resolve conflicts

--- src/app/rating/rating.model.ts
+++ src/app/rating/rating.model.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export interface Rating {
   currentUserId: any;
   selectedUserId?: any;
@@ -6,12 +6,4 @@ export interface Rating {
   selectedUserName?: any;
   value?: number;
 }
-=======
-export interface Rating {
-  currentUserId: any;
-  selectedUserId?: any;
-  currentUserName?: any;
-  selectedUserName?: any;
-  value?: number;
-}
->>>>>>> d3a6557c6445a9da6c19f225f715752a4889f955
+
